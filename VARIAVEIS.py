b4344f39b371dd3628fc759f98f9346d02a08f34
larinhax1 <larafelisbertosenai@gmail.com>
2024-04-23 16:51:58 -0300

    criação variaveis

---
+++ VARIAVEIS.py
@@ -0,0 +1,12 @@
+nome="Lara Felisberto da Silva"
+idade=16
+email='larafelisbertosenai@gmail.com'
+telefone='17996442510'
+nota=10
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
