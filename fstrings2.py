0d88c417bdbde39f4383f7382d253d448877fbb0
larinhax1 <larafelisbertosenai@gmail.com>
2024-05-02 17:05:35 -0300

    Criação fstrings2.py

--- fstrings2.py
+++ fstrings2.py
@@ -1,7 +1,11 @@
 #Formatação de Strings com várias linhas
 escola = 'Senai'
 curso = 'Desenvolvimento de Sistemas'
+matricula = 34
+nota = 8.99999
 uc = 'Lógica de Programação'
 print(f"Escola: {escola}\n"
       f"Curso: {curso}\n"
-      f"Unidade Curricular: {uc}")
+      f"Unidade Curricular: {uc}\n"
+      f"Matrícula: {matricula:06d}\n"
+      f"Nota: {nota:.2f}")
